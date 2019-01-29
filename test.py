e45fb402f82b870ab14330c6601777372d05447f
CaptAkashKris <akashkrishnapm@cet.ac.in>
2019-01-30 04:32:33 +0530

    okk

---
+++ test.py
@@ -0,0 +1,96 @@
+    1  history 
+    2  uname
+    3  uname -a
+    4  tree
+    5  sudo apt install tree
+    6  tree
+    7  cd ..
+    8  tree
+    9  cd ..
+   10  tree
+   11  cd
+   12  echo hello world
+   13  touch hello
+   14  echo "hello world"> hello
+   15  cat hello
+   16  echo "hello world hai">> hello
+   17  cat hello
+   18  echo "hello world hai"> hello
+   19  cat hello
+   20  ls > hello
+   21  cat hello
+   22  ls
+   23  echo something
+   24  cat something
+   25  something > hello
+   26  ls -l
+   27  ls -l > hello
+   28  cat hello
+   29  uname > hello
+   30  cat hello
+   31  nano pgm.sh
+   32  pgm.sh
+   33  bash pgm.sh
+   34  ls -l
+   35  ls -l pgm.sh
+   36  sudo chmod +x pgm.sh 
+   37  ls -l pgm.sh
+   38  sudo chmod -x pgm.sh 
+   39  ls -l pgm.sh
+   40  sudo chmod +x pgm.sh 
+   41  pgm.sh
+   42  ./pgm.sh
+   43  sudo apt uninstall tree
+   44  curl https://user9747.github.io/Foss/
+   45  sudo apt install curl
+   46  curl https://user9747.github.io/Foss/
+   47  man git
+   48  man Git
+   49  makdir csfoss
+   50  mkdir csfoss
+   51  cd csfoss
+   52  git
+   53  sudo apt install git
+   54  git init
+   55  git config --global user.name "CaptAkashKris"
+   56  touch init
+   57  git config --global user.email "akashkrishnapm@cet.ac.in"
+   58  git init
+   59  git add
+   60  git add .
+   61  git commit -m "Commit Message"
+   62  git remote add origin "url at github"
+   63  git push -u origin master
+   64  git init
+   65  git push -u origin master
+   66  git remote add origin "https://github.com/CaptAkashKris/csfoss.git"
+   67* 
+   68  git remote add origin "https://github.com/CaptAkashKris/csfoss.git"
+   69  sudo t -r .git
+   70  git init
+   71  history
+   72  git add .
+   73  git commit -m "enthokkanavo"
+   74  git commit -m "first commit"
+   75  git commit -m "enthokkanavo"
+   76  git remote add origin "url at github"
+   77  git remote add origin "https://github.com/CaptAkashKris/csfoss.git"
+   78  cd
+   79  mkdir project1
+   80  cd project1/
+   81  touch file1.a
+   82  touch file2.a
+   83  git init
+   84  ls -la
+   85  git status
+   86  git add .
+   87  git status
+   88  git commit -m "first commit adding 2 files"
+   89  ls
+   90  git remote add origin "https://github.com/CaptAkashKris/puthiya.git"
+   91  git commit -m "first commit adding 2 files"
+   92  git status
+   93  git push -u origin master 
+   94  nano test.py
+   95  git status
+   96  history > test.py
